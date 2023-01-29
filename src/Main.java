6e877322e24b8b043578f26db905c29baea8ba43
Artsiom Paulenka <pavlenkoartem611@gmail.com>
2023-01-29 15:20:01 +0100

    homework

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[]array = {3, 6, 4, 7, 2, 1, 9};
+        int number = 10 ;
+
+        int result = Array.arrayResult(array,number);
+        System.out.println(result);
+
+    }
+}
